470604e7020ce7e042593ca40565dfa573e91343
Stefan Schäfer <stefan@invis-server.org>
2014-08-23 13:44:32 +0200

    dokuwiki setup in sine an AD angepasst

--- usr/share/doc/packages/invisAD-setup/examples/dokuwiki/local.php
+++ usr/share/doc/packages/invisAD-setup/examples/dokuwiki/local.php
@@ -37,6 +37,6 @@ $conf['plugin']['authad']['domain_controllers'] = 'localhost';
 $conf['plugin']['authad']['admin_username'] = 'ldap.admin';
 $conf['plugin']['authad']['admin_password'] = 'admin-secret';
 $conf['plugin']['authad']['use_ssl'] = 0;
-$conf['plugin']['authad']['mapping']['name'] = 'dislayname';
+$conf['plugin']['authad']['mapping']['name'] = 'displayname';
 
 ?>
