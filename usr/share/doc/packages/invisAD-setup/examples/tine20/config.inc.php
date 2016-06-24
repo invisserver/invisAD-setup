c793e95da4c5314fea345a59e2a1552eb2ec4bf1
invis Team osc16 <info@invis-server.org>
2016-06-24 16:42:05 +0200

    tine20 Setup in sine erweitert.

---
+++ usr/share/doc/packages/invisAD-setup/examples/tine20/config.inc.php
@@ -0,0 +1,17 @@
+<?php
+  return array(
+    'database' => array(
+      'host'        => 'localhost',
+      'dbname'      => 'tine20',
+      'username'    => 'tine20',
+      'password'    => 'tinedbpw',
+      'port'        => '3306',
+      'adapter'     => 'pdo_mysql',
+      'tableprefix' => 'tine20_',
+    ),
+    'setupuser' => array(
+      'username'    => 'tine20setup',
+      'password'    => 'setup'
+    ),
+  );
+?>
