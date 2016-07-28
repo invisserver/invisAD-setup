d68b31bf7ea38c28a793354f7e21523f0cbbb2c4
Stefan Schaefer <stefan@invis-server.org>
2016-07-28 19:42:18 +0200

    Bugfix: CorNAz erhält den Status "anwesend" jetzt wieder, wenn ein Konto hinzugefügt wird.

--- srv/www/htdocs/cornaz/excreateacc.php
+++ srv/www/htdocs/cornaz/excreateacc.php
@@ -109,7 +109,7 @@ if ( $status == "Anwesend" ) {
 		$r=ldap_bind($ditcon,$LDAP_BIND_DN, "$LDAP_BIND_PW");
 		$filter="(&(fspExtMailServer=*)(fspLocalMailAddress=$corusername*))";
 		$justthese = array( "fspExtMailAddress", "fspExtMailProto", "fspExtMailUsername", "fspExtMailServer", "fspExtMailUserPw", "fspMailfetchOpts");
-		$sr=ldap_search($ditcon, $dn, $filter, $justthese);
+		$sr=ldap_search($ditcon, $COR_LDAP_SUFFIX, $filter, $justthese);
 		$entries = ldap_get_entries($ditcon, $sr);
 		#	print $entries["count"]." Einträge gefunden<p>";
 		ldap_close($ditcon);
