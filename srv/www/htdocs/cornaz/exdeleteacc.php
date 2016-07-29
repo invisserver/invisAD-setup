7b1ba62e06eef3b6a2f41607ffafdfceee448da5
Stefan Schaefer <stefan@invis-server.org>
2016-07-29 07:41:19 +0200

    Bugfix: CorNAz bereinigt fie fetchmailrc jetzt auch wieder beim Löschen eines Accounts.

--- srv/www/htdocs/cornaz/exdeleteacc.php
+++ srv/www/htdocs/cornaz/exdeleteacc.php
@@ -39,7 +39,7 @@ if ( $status == "Anwesend" ) {
 		fwrite ($fh, "$zeile");
 	}
 	fclose($fh);
-	exec ("sudo $COR_PATH/fetchcopy");
+	exec ("sudo $COR_PATH/bin/fetchcopy");
 	// Verbindung zum LDAP Server aufbauen
 	$ditcon=ldap_connect("$LDAP_SERVER");
 	// LDAP Protokoll auf Version 3 setzen
@@ -50,8 +50,6 @@ if ( $status == "Anwesend" ) {
 
 	// Am LDAP per SimpleBind anmelden
 	if ($ditcon) {
-		// bind mit passendem dn für aktulisierenden Zugriff
-		//$dn=("uid=$corusername,$BASE_DN_USER");
 		$r=ldap_bind($ditcon,$LDAP_BIND_DN, "$LDAP_BIND_PW");
 		$filter="(&(fspExtMailServer=*)(fspLocalMailAddress=$corusername*))";
 		$justthese = array( "fspExtMailAddress", "fspExtMailProto", "fspExtMailUsername", "fspExtMailServer", "fspExtMailUserPw", "fspMailfetchOpts");
@@ -77,7 +75,7 @@ if ( $status == "Anwesend" ) {
 		fclose($fh);
 		$i++;
 	}
-	exec ("sudo $COR_PATH/fetchcopy");
+	exec ("sudo $COR_PATH/bin/fetchcopy");
 	$ausgabe = "<b>Status:</b> Das regelmäßige Abrufen Ihrer eMails wurde für folgende Adressen aktiviert:<p>";
 	$i=0;
 	foreach ($entries as $zugangsdaten) {
