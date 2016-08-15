089cc7894b2e2bd4d32a048f6a21f938e89d1309
Stefan Schäfer <stefan@invis-server.org>
2016-08-15 16:26:18 +0200

    Tine20:
    - sine an neue tine20 Version angepasst.
    - neue tine20 config.inc.php in examples eingefuegt.

--- usr/share/doc/packages/invisAD-setup/examples/tine20/config.inc.php
+++ usr/share/doc/packages/invisAD-setup/examples/tine20/config.inc.php
@@ -1,17 +1,41 @@
 <?php
-  return array(
-    'database' => array(
-      'host'        => 'localhost',
-      'dbname'      => 'tine20',
-      'username'    => 'tine20',
-      'password'    => 'tinedbpw',
-      'port'        => '3306',
-      'adapter'     => 'pdo_mysql',
-      'tableprefix' => 'tine20_',
-    ),
-    'setupuser' => array(
-      'username'    => 'tine20setup',
-      'password'    => 'setup'
-    ),
-  );
-?>
+return array (
+  'database' =>
+  array (
+    'host' => 'localhost',
+    'dbname' => 'tine20',
+    'username' => 'tine20',
+    'password' => 'tinedbpw',
+    'adapter' => 'pdo_mysql',
+    'tableprefix' => 'tine20_',
+    'port' => '3306',
+  ),
+  'setupuser' =>
+  array (
+    'username' => 'tine20setup',
+    'password' => 'setup',
+  ),
+  'logger' =>
+  array (
+    'active' => false,
+    'filename' => '/var/log/apache2/tine20.log',
+    'priority' => 6,
+  ),
+  'caching' =>
+  array (
+    'active' => true,
+    'path' => '/var/lib/tine20/tmp',
+    'lifetime' => 120,
+  ),
+  'session' =>
+  array (
+    'path' => '/var/lib/tine20/session',
+    'lifetime' => 86400,
+    'backend' => 'File',
+    'host' => 'localhost',
+    'port' => 6379,
+  ),
+  'tmpdir' => '/var/lib/tine20/tmp',
+  'filesdir' => '/var/lib/tine20/files',
+  'mapPanel' => 1,
+);
