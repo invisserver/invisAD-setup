200823334a92bf0105ce475e15a386c62df9dda8
Marcel Richter <wmrichter@gmail.com>
2021-08-14 17:01:21 +0200

    portal phpstan lvl 1

--- srv/www/htdocs/portal/script/ping.php
+++ srv/www/htdocs/portal/script/ping.php
@@ -3,15 +3,16 @@
  * AJAX script, pinging a host an returning status as images (HTML-tag)
  * (C) 2009 Daniel T. Bender, invis-server.org
  * (C) 2013 Ingo Göppert, invis-server.org
+ * (C) 2021 W.-Marcel Richter, invis-server.org
  * License GPLv3
  * Questions: daniel@invis-server.org
  */
 if (!isset($_COOKIE['invis'])) die();
 
+$IP='';
 if (isset($_POST['ip'])) $IP = $_POST['ip'];
-
-exec("ping -a -c3 -n -q $IP;", $null, $return);
-// 0: online, 1: unreachable, 2: error
+if (!empty($web)){
+    /* 0: online, 1: unreachable, 2: error */
+    exec("ping -a -c3 -n -q $IP;", $null, $return);
+} else { $return = 2; }
 echo ($return === 0)?"<img title='IP: $IP' src='images/ok.png' />":"<img title='IP: $IP' src='images/not_ok.png' />";
-
-?>
